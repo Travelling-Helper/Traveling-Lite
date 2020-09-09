b1199f023f73f11d4144c7540eb3ec3840b3b729
fredhu0514 <huyuqi0514@126.com>
2020-09-09 17:18:37 +0800

    init with algo_one_path

---
+++ src/mpath/models.py
@@ -0,0 +1,16 @@
+"""
+@author Li Tian & Yuqi Hu
+@date Sep. 9th 2020
+@version 0.0
+"""
+
+from django.db import models
+from one_path_algo import one_path_algo as algo
+"""
+dict = {'a': 3, 'b': 0, 'c': 9, 'h': -10}
+func = algo.argmin
+result = func(dict)
+>>> ('h', -10)
+"""
+
+# Create your models here.
