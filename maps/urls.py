0466f3be40c510bccd5362c63cff3088cd0528f9
fredhu0514 <huyuqi0514@126.com>
2020-09-12 21:22:01 +0800

    err 404

--- maps/urls.py
+++ maps/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 import maps.views
 
 urlpatterns = [
-    path('robots.txt', maps.views.robots),
+    path('robots.txt', maps.views.map_robots),
     path('map', maps.views.gaodeMap)
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 # Robots.txt for crawler engines
 def robots(request):
     return render(request, "robots.txt")
+def map_robots(request):
+    return render(request, "err_404.html")
 
 def gaodeMap(request):
     return render(request, "map.html")
