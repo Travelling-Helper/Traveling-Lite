0f762f5e3c378b4d6c1dda900316ff88d8aec79d
fredhu0514 <huyuqi0514@126.com>
2020-09-12 14:35:43 +0800

    maps

--- maps/urls.py
+++ maps/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 import maps.views
 
 urlpatterns = [
-    path('robots.txt',maps.views.robots),
-    path('map', maps.views.gaodeMap)
+    path('robots.txt', maps.views.robots),
+    path('', maps.views.gaodeMap)
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
