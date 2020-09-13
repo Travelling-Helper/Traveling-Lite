328eb7ec3320e2a26d40d2bf1d98224e64ca7b7c
fredhu0514 <huyuqi0514@126.com>
2020-09-13 20:52:53 +0800

    format stuff

--- Traveling_Lite/urls.py
+++ Traveling_Lite/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, re_path, include
 from django.shortcuts import redirect
 from maps import views as map_views
 
